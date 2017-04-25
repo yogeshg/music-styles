d75e67178e3c54d72230ff3815152306f2f12554
Yogesh Garg (Clic) <yg2482@columbia.edu>
2017-04-25 15:26:29 -0400

    updating midi2chord.py, @goddenrich please review this

--- src/midi2chord.py
+++ src/midi2chord.py
@@ -1,14 +1,16 @@
+import logging
+logging.basicConfig(level = logging.INFO , format=
+        '%(asctime)s:%(levelname)s:%(name)s:%(threadName)s:line %(lineno)d: %(message)s')
+logger = logging.getLogger(__name__)
+
 import sys
 import cPickle
 import argparse
-import logging
 import os
 import subprocess
 import json
 import urllib
 import tarfile
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 def load_csv(filename):
     import csv
@@ -99,19 +101,23 @@ def allcsvs(l_root, msd_id, save_dir):
     for l_id, csv in msd_id_to_dirs(l_root, msd_id, save_dir):
         yield (l_id, csv)
 
+def executeBashCommand(bashCommand):
+    logger.debug('trying to execute:'+bashCommand)
+    return subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+
 def midi2csv(p,midifname, save_dir):
-    path = os.path.join(p,midifname)
-    csv_file_path = os.path.join(save_dir, '/tmp/test.csv')
+    midipath = os.path.join(p,midifname)
+    csvpath = '/tmp/test.csv'
     l_id = midifname
-    bashCommand = "midicsv %s %s" % (path, csv_file_path)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    bashCommand = "{} {} {}".format(args.midicsv_exe, midipath, csvpath)
+    process = executeBashCommand( bashCommand )
     output, error = process.communicate()
-    return l_id, csv_file_path
+    return l_id, csvpath
 
 def deleteCsv(csvfname):
     #deletes csvfname
     bashCommand = "rm %s" % csvfname
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    process = executeBashCommand( bashCommand )
     output, error = process.communicate()
     return
 
@@ -134,7 +140,9 @@ def loadLabels(strat, genres):
                     pass
     return labels
 
-def classPkl(data_root, save, noclip=True, valid_cut = 0.2, number_files=100):
+from collections import defaultdict, OrderedDict, Counter
+
+def classPkl(data_root, save, noclip=True, valid_cut = 0.2, number_files=None):
     checkDataExist(data_root, save)
     stratSplit=os.path.join(data_root,'msd-topMAGD-partition_percentageSplit_0.8-v1.0.cls')
     genres=os.path.join(data_root,'msd-topMAGD-genreAssignment.cls')
@@ -146,6 +154,7 @@ def classPkl(data_root, save, noclip=True, valid_cut = 0.2, number_files=100):
     trainMeta=[]
     testMeta=[]
     i=1
+    counter = Counter()
     for (m_id, genre, isTest) in loadLabels(stratSplit, genres):
         for l_id, csvname in allcsvs(l_root, m_id, save):
             # at this point csvname will exist
@@ -161,9 +170,14 @@ def classPkl(data_root, save, noclip=True, valid_cut = 0.2, number_files=100):
                 testLabels.append(genre)
                 testMeta.append([m_id, l_id, genre, isTest])
             if i % 50==1:
-                print 'processing file', i
+                logger.info('processing file'+str(i))
             i+=1
-        if i>number_files:
+            counter['num_csvs']+=1
+            counter[('num_csvs',isTest)]+=1
+            counter[('genre',genre)]+=1
+            counter[('genre',isTest,genre)]+=1
+
+        if (not number_files is None) and (i>number_files):
             break
     
     l = len(trainMeta)
@@ -171,48 +185,58 @@ def classPkl(data_root, save, noclip=True, valid_cut = 0.2, number_files=100):
     data = {'train': trainSongs[:l1], 'valid':trainSongs[l1:], 'test':testSongs}
     labels = {'train': trainLabels[:l1], 'valid': trainLabels[l1:], 'test':testLabels}
     meta = {'train': trainMeta[:l1], 'valid': trainMeta[l1:], 'test':testMeta}
+    stats = OrderedDict()
+    stats['train_length'] = len(data['train'])
+    stats['valid_length'] = len(data['valid'])
+    stats['test_length'] = len(data['test'])
+    stats['counter'] = {str(k):v for k,v in counter.iteritems()}
         
     if save:
-        print 'saving pkl and json files to', save
+        logging.info('saving pkl and json files to'+str(save))
         with open(os.path.join(save,'X.pickle'),'wb') as f:
             cPickle.dump(data, f)
         with open(os.path.join(save,'y.pickle'),'wb') as f:
             cPickle.dump(labels, f)
         with open(os.path.join(save,'metadata.json'),'w') as f:
-            json.dump(meta, f)
+            json.dump(meta, f, indent=2)
+        with open(os.path.join(save,'stats.json'), 'w') as f:
+            json.dump(stats, f, indent=2)
+            
 
     return data, labels, meta
 
 def checkDataExist(data_root, save):
     if not os.path.exists(data_root):
-        print "making data_root"
+        logging.info("making data_root")
         os.makedirs(data_root)
     if not os.path.exists(save):
-        print "making save"
+        logging.info("making save")
         os.makedirs(save)
-    if not os.path.exists(os.path.join(save,'tmp')):
-        print "makeing tmp"
-        os.makedirs(os.path.join(save,'tmp'))
+    # /tmp/ is a default directory for such things
+    # if not os.path.exists(os.path.join(save,'tmp')):
+    #     logging.info("makeing tmp")
+    #     os.makedirs(os.path.join(save,'tmp'))
     l_path = os.path.join(data_root, 'lmd_aligned')
     if not os.path.exists(l_path):
         lzip= os.path.join(data_root, 'lmd_aligned.tar.gz')
         if not os.path.exists(lzip):
-            print 'lakh database zip doesnt exist in root data downloading it'
+            logging.info('lakh database zip doesnt exist in root data downloading it')
             urllib.urlretrieve('http://hog.ee.columbia.edu/craffel/lmd/lmd_aligned.tar.gz', lzip)
-        print 'please go to the directory %s and unzip lmd_aligned.tar.gz before running the program again' % data_root
-        sys.exit()
+        logging.info('please go to the directory %s and unzip lmd_aligned.tar.gz before running the program again' % data_root)
+        sys.exit() # TODO @RG sys.exit shouldn't be thrown from code, you should throw exceptions, and catch them at top level to exit if that makes sense
+        # this makes the code unusable from ipython notebooks for example.
     part_path = os.path.join(data_root,'msd-topMAGD-partition_percentageSplit_0.8-v1.0.cls')
     genre_path = os.path.join(data_root,'msd-topMAGD-genreAssignment.cls')
     if not os.path.exists(genre_path):
-        print 'genre labels file doesnt exist downloading it:'
+        logging.info('genre labels file doesnt exist downloading it')
         urllib.urlretrieve('http://ifs.tuwien.ac.at/mir/msd/partitions/msd-topMAGD-genreAssignment.cls', genre_path)
     if not os.path.exists(part_path):
-        print 'stratified split file doesnt exist downloading it:'
+        logging.info('stratified split file doesnt exist downloading it')
         urllib.urlretrieve('http://ifs.tuwien.ac.at/mir/msd/partitions/msd-topMAGD-partition_percentageSplit_0.8-v1.0.cls', part_path)
-    print 'done ensuring data set up properly'
+    logging.info('done ensuring data set up properly')
 
 
-
+# TODO: @RG remove this!!
 def embeddingPkl(data_root, save=None, noclip=True, train_cut=0.8, valid_cut=0.2, number_files=100):
     logger.debug(str(locals()))
     checkDataExist(data_root, save)
@@ -251,21 +275,22 @@ def embeddingPkl(data_root, save=None, noclip=True, train_cut=0.8, valid_cut=0.2
 
     return data, meta
 
-def main(data_root, save=None, noclip=True, train_cut=0.6, valid_cut = 0.2, number_files=100, embedding=False):
-    if embedding:
-        embeddingPkl(data_root, save, noclip, train_cut, valid_cut, number_files)
-    else:
+def main(data_root, save=None, noclip=True, train_cut=0.6, valid_cut = 0.2, number_files=100, **kwargs):
         classPkl(data_root, save, noclip, valid_cut, number_files)
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser()
+    __example__ = 'python src/midi2chord.py --data_root data/ --save data/lmd_20170425/ --number_files 10 --midicsv_exe /home/yg2482/code/music-styles/midicsv-1.1/midicsv'
+    parser = argparse.ArgumentParser(description='example:\n\t'+__example__)
     parser.add_argument('--data_root', required=True, type=str, help='specify the directory containing the lakh dataset, the labels and the stratified split')
     parser.add_argument('--save', required=True, type=str, help='specify what directory to save the outputs to')
     parser.add_argument('--noclip', default=False, action='store_true', help='use this flag to not clip the range of notes in chords to 88 range of chord2vec')
     parser.add_argument('--train_cut', type=float, default=0.6, help='fraction of data to be used for training')
     parser.add_argument('--valid_cut', type=float, default=0.2, help='fraction of training data to be used for validation')
-    parser.add_argument('--number_files', type=int, default=100, help='the approximate number of data examples to create')
-    parser.add_argument('--embedding', default=False, action='store_true', help='use this flag to save in the format for embedding training')
+    parser.add_argument('--number_files', type=int, default=None, help='the approximate number of data examples to create')
+    parser.add_argument('--midicsv_exe', type=str, default='midicsv', help='path to midicsv executable')
+    global args
     args = parser.parse_args(sys.argv[1:])
+    logger.info(str(args.__dict__))
     
     main(**args.__dict__)
+
