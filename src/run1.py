1cc5209c098b73e70e76954d706a9877fd608562
Richard Godden <goddenrich@gmail.com>
2017-05-04 16:36:46 -0400

    auto commit tracked files

--- src/run1.py
+++ src/run1.py
@@ -2,9 +2,9 @@ from models import *
 
 def main():
     commit_hash = save_code()
-    embeddings_path = '/home/yg2482/code/chord2vec/data/chord2vec_199.npz'
-    x_datapath='data/X.pickle'
-    y_datapath='data/y.pickle'
+    embeddings_path = '../../../chord2vec/data/chord2vec_199.npz'
+    x_datapath='data/tmp/X.pickle'
+    y_datapath='data/tmp/y.pickle'
     load_embeddings(embeddings_path=embeddings_path)
     load_data(x_datapath=x_datapath, y_datapath=y_datapath)
     embeddings=False
