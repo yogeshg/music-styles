ecc2facda230e4e89d0ba986f6daea09b8113e2b
Richard Godden <goddenrich@gmail.com>
2017-05-05 13:21:02 -0400

    auto commit tracked files

--- src/run1.py
+++ src/run1.py
@@ -7,7 +7,7 @@ def main():
     y_datapath='../data/tmp/y.pickle'
     load_embeddings(embeddings_path=embeddings_path)
     load_data(x_datapath=x_datapath, y_datapath=y_datapath)
-    embeddings=False
+    embeddings=True
     run_experiment(**locals())
 
 main()
